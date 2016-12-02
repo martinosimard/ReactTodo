ea43999a1cc5967a833e3d9c04e3fad08579c55c
Martin Simard <Martin Simard>
2016-12-02 08:38:54 -0500

    Style the application

--- app/components/AddTodo.jsx
+++ app/components/AddTodo.jsx
@@ -14,10 +14,10 @@ var AddTodo = React.createClass({
   },
   render : function () {
     return (
-      <div>
+      <div className="container__footer">
         <form ref="form" onSubmit={this.onSubmit}>
           <input type="text" ref="todo" placeholder="What do you need to do?" />
-          <button className="button">Add to do</button>
+          <button className="button expanded">Add to do</button>
         </form>
     </div>
     )
--- app/tests/components/TodoList.test.jsx
+++ app/tests/components/TodoList.test.jsx
@@ -30,4 +30,14 @@ describe('TodoList', () => {
 
   })
 
+  it('should render empty messahe if no todos', () => {
+    var todos = [];
+
+    var todoList = TestUtils.renderIntoDocument(<TodoList todos={todos}/>)
+    var todosComponents = TestUtils.scryRenderedComponentsWithType(todoList, Todo);
+    var $el = $(ReactDOM.findDOMNode(todoList));
+
+    expect($el.find('.container__message').length).toBe(1);
+  })
+
 });
